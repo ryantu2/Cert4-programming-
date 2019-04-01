fbf72db373372fee70d744ac1142983717d5dcbb
ryantu2 <48039123+ryantu2@users.noreply.github.com>
2019-04-01 16:08:20 +1100

    NPC dialogue
    npc

---
+++ Debug/Assets/HeartHealth.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[AddComponentMenu("Intro PRG/RPG/Player/Health - Heart")]
+public class HeartHealth : MonoBehaviour
+{
+    [Header("Player Stats")]
+    //public maxHealth
+    public int maxHealth;
+    //public curHealth
+    public float curHealth;
+    [Header("Heart Slots")]
+    //Canvas Image heartSlots array
+    public Image[] heartSlots;
+    //Sprite hearts array
+    public Sprite[] hearts;
+    //private percent healthPerSection
+    private float healthPerSection;
+    #region Start
+    private void Start()
+    {
+        UpdateHearts();
+    }
+    #endregion
+    #region Update 
+    private void Update()
+    {
+        int i = 0;
+        foreach (Image slot in heartSlots)
+        {
+            if (curHealth >= ((healthPerSection * 4)) + healthPerSection * 4 * i)
+            {
+                heartSlots[i].sprite = hearts[0];
+            }
+            else if (curHealth >= ((healthPerSection * 3)) + healthPerSection * 4 * i)
+            {
+                heartSlots[i].sprite = hearts[1];
+
+            }
+            else if (curHealth >= ((healthPerSection * 2)) + healthPerSection * 4 * i)
+            {
+                heartSlots[i].sprite = hearts[2];
+            }
+            else if (curHealth >= ((healthPerSection * 1)) + healthPerSection * 4 * i)
+            {
+                heartSlots[i].sprite = hearts[3];
+            }
+            else 
+            {
+                heartSlots[i].sprite = hearts[4];
+            }
+            i++;
+        }
+
+    }
+    #endregion
+    #region UpdateHearts
+    private void UpdateHearts()
+    {
+        healthPerSection = maxHealth / (heartSlots.Length * 4);
+    }
+    #endregion
+}
+//errors 18
---
+++ Movement/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public bool showDlg, grid;
+    public Vector2 scr;
+    public string[] dlgText;
+    public int index, optionsIndex;
+
+    private void OnGUI()
+    {
+        if (showDlg)
+        {
+            if(scr.x != Screen.width/16 || scr.y != Screen.height / 9)
+            {
+                scr.x = Screen.width / 16;
+                scr.y = Screen.height / 9;
+            }
+            GUI.Box(new Rect(0,6*scr.y,Screen.width,3*scr.y), dlgText[index]);
+          
+            //!index+1 >=dlgText.Length
+            //!index >= dlgText.Length-1
+            //index < dlgText.Length
+            if (!(index + 1 >= dlgText.Length || index ==optionsIndex))
+            {
+                if(GUI.Button(new Rect(scr.x*15,scr.y*8.5f,scr.x,scr.y*0.5f), "Next"))
+                {
+                    index++;
+                }
+            }
+            else if (index == optionsIndex)
+            {
+                if(GUI.Button(new Rect(scr.x * 13, scr.y * 8.5f, scr.x, scr.y * 0.5f), "Accept"))
+                {
+                    index++;
+                }
+                if(GUI.Button(new Rect(scr.x * 14, scr.y * 8.5f, scr.x, scr.y * 0.5f), "Decline"))
+                {
+                    index = dlgText.Length - 1;
+                }
+
+            }
+            else
+            {
+                if (GUI.Button(new Rect(scr.x * 15, scr.y * 8.5f, scr.x, scr.y * 0.5f), "bye"))
+                {
+                    index = 0;
+                    showDlg = false;
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                    //enable player and camera movement
+                }
+            }
+
+        }
+       
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
